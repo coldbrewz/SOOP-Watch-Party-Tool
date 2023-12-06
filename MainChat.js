67f037d7cdba742dde977ace1b1eb3484c5a867e
Escaleirex <69918820+Escaleirex@users.noreply.github.com>
2023-12-06 15:34:11 +0000

    AAAAAAAAAAAAAAA

--- MainChat.js
+++ MainChat.js
@@ -27,6 +27,13 @@ window.addEventListener("load", (event) => {
     document.getElementById("Afreeca").height = h
 
     const url = getCurrentURL()
+
+    document.getAnimations(link, function(response){
+        var html = response
+        var html_src = 'data:text/html;charset=utf-8,' + html
+        document("#Twitch").attr("src", html_src)
+    })
+
     document.getElementById("Twitch").src = "https://player.twitch.tv/?channel=" + url.substring(8) + "&parent=https://github.io"
     document.getElementById("ChatEmbed").src = "https://www.twitch.tv/embed/" + url.substring(8) + "/chat?darkpopout&parent=https://github.io"
     if((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
